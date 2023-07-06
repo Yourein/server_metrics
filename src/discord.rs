593d6b14db3dc23c62d1a06463f029b1a31b9879
Yourein <rein.you.3.1@gmail.com>
2023-07-06 12:35:16 +0900

    Success to post to discord with webhook. We need to implement formatting SystemInfo struct to webhook format.

---
+++ src/discord.rs
@@ -0,0 +1,57 @@
+use reqwest::blocking::Client as reqwestClient;
+use serde::Serialize;
+
+//Insert webhook url before build
+const WHURL: &str = "";
+
+#[derive(Serialize)]
+struct PostTest {
+    username: String,
+    content: String
+}
+
+#[derive(Serialize)]
+struct Post {
+    username: String,
+    embeds  : Vec<Embed>
+}
+
+#[derive(Serialize)]
+struct Embed {
+    title : String,  //title of embed
+    fields: Vec<Field>
+}
+
+#[derive(Serialize)]
+struct Field {
+    name: String,
+    value: String,
+    inline: bool
+}
+
+pub fn post_webhook() -> Result<(), String> {
+    let client = reqwestClient::new();
+
+    let payload = Post {
+        username: "tester".to_string(),
+        embeds: vec![Embed {
+            title: "Test".to_string(),
+            fields: vec![
+                Field {name: "f1".to_string(), value: "v1".to_string(), inline: true},
+                Field {name: "f2".to_string(), value: "v2".to_string(), inline: true}
+            ]
+        }]
+    };
+
+    if let Ok(res) = client.post(WHURL).json(&payload).send() {
+        if res.status().as_u16() == 204 {
+            Ok(())
+        }
+        else {
+            Err(format!{"Resp: {}", res.text().unwrap()})
+        }
+    }
+    else {
+        Err("Could not send the post request".to_string())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 mod systeminfo;
+mod discord;
 
 fn main() {
     if !systeminfo::is_supported() {
         panic!{"This system is not supported! Exiting..."};
     }
 
-    println!{"{:?}", systeminfo::fetch_info()};
+    let res = discord::post_webhook();
+    match res {
+        Ok(()) => println!{"OK"},
+        Err(e) => println!{"{}", e}
+    }
 }
