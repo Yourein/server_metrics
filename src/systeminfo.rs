e503926ce4e63a3b2dfb418c7c9dbb0794fcfd43
Yourein <rein.you.3.1@gmail.com>
2023-07-07 14:15:55 +0900

    Change how to fetch the cpu usage

--- src/systeminfo.rs
+++ src/systeminfo.rs
@@ -8,7 +8,7 @@ pub struct SystemInfo {
     pub storage_used    : u64, //Used storage as Byte
     pub storage_free    : u64, //Free storage as Byte
     pub storage_usage   : f32, //Percentage of used storage over total
-    pub global_cpu_usage: f32  //Percentage of usage of CPU socket
+    pub global_cpu_usage: f32  //Percentage of usage of CPU
 }
 
 pub fn is_supported() -> bool {
@@ -17,8 +17,20 @@ pub fn is_supported() -> bool {
 
 pub fn fetch_info() -> SystemInfo {
     let mut sys = System::new_all();
-    sys.refresh_all();
 
+    println!{"Fetching cpu info..."};
+    //We do blocking here since cpu_usage() use differential information from past.
+    //So, to fetch collect cpu usage value, we need to loop at least twice.
+    //We looping 3 times for more accurate value
+    let mut global_cpu_usage: f32 = 0.0;
+    for _ in 0..3 {
+        std::thread::sleep(std::time::Duration::from_millis(500));
+        sys.refresh_cpu();
+        global_cpu_usage = sys.global_cpu_info().cpu_usage();
+    }
+
+    println!{"Fetching storage and memory info..."};
+    sys.refresh_all();
     let disk = &sys.disks()[0];
     let disk_used = disk.total_space() - disk.available_space();
 
@@ -29,6 +41,6 @@ pub fn fetch_info() -> SystemInfo {
         storage_used: disk_used,
         storage_free: disk.available_space(),
         storage_usage: (disk_used as f32) * 100.0 / disk.total_space() as f32,
-        global_cpu_usage: sys.global_cpu_info().cpu_usage()
+        global_cpu_usage: global_cpu_usage
     }
 }
